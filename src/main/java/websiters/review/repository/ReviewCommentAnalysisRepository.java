53fb676375ccbcb8bfe9ca1ff74a0ffbdf82fc63
Luis Ángel 10 <luisangelmartinelopez56@gmail.com>
2025-11-30 22:48:53 -0600

    repository

--- src/main/java/websiters/review/repository/ReviewCommentAnalysisRepository.java
+++ src/main/java/websiters/review/repository/ReviewCommentAnalysisRepository.java
@@ -7,5 +7,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface ReviewCommentAnalysisRepository extends JpaRepository<ReviewCommentAnalysis, UUID> {
-    Optional<ReviewCommentAnalysis> findByComment_Id(UUID commentId);
+
+    Optional<ReviewCommentAnalysis> findByCommentId(UUID commentId);
+
 }
