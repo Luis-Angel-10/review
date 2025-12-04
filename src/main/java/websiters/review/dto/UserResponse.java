7e2ec8b7d9a1fef5624cbfff9cec0c7235144a4b
Luis Ángel 10 <luisangelmartinelopez56@gmail.com>
2025-12-04 09:42:39 -0600

    UUID

--- src/main/java/websiters/review/dto/UserResponse.java
+++ src/main/java/websiters/review/dto/UserResponse.java
@@ -2,9 +2,11 @@ package websiters.review.dto;
 
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 public class UserResponse {
-    private Long id;
+    private UUID id;
     private String email;
     private boolean active;
 }
